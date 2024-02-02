50f2286881d8cd64975626eb1da19cb40159c37f
Ivan Bravo <ivnbrvsnchz@gmail.com>
2024-02-02 19:32:06 +0100

    vitest install

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  server: {
+    port: 80,
+    host: "0.0.0.0",
+    cors: true,
+  },
 });
