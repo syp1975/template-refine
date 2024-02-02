dc0213831602f50b6c20cf56e9d6ef7c4c0c4fd9
Ivan Bravo <ivnbrvsnchz@gmail.com>
2024-02-02 21:12:49 +0100

    es language

--- src/i18n.ts
+++ src/i18n.ts
@@ -8,13 +8,13 @@ i18n
   .use(detector)
   .use(initReactI18next)
   .init({
-    supportedLngs: ["en", "de"],
+    supportedLngs: ["en", "es"],
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
     ns: ["common"],
     defaultNS: "common",
-    fallbackLng: ["en", "de"],
+    fallbackLng: ["en", "es"],
   });
 
 export default i18n;
